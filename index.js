4f95e4a0ef46b3d09715f9fdf549ceb267622c31
brandiw <brandimichelle.0911@gmail.com>
2019-01-29 22:28:55 -0800

    Removing body-parser

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
 const expressJWT = require('express-jwt');
@@ -14,18 +13,18 @@ const app = express();
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(cors());
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({extended: false}));
 
 // Helper function: This allows our server to parse the incoming token from the client
 // This is being run as middleware, so it has access to the incoming request
 function fromRequest(req){
-    if(req.body.headers.Authorization &&
-      req.body.headers.Authorization.split(' ')[0] === 'Bearer'){
-      return req.body.headers.Authorization.split(' ')[1];
-    }
-    return null;
+  if(req.body.headers.Authorization &&
+    req.body.headers.Authorization.split(' ')[0] === 'Bearer'){
+    return req.body.headers.Authorization.split(' ')[1];
   }
+  return null;
+}
 
 // Controllers
 // All auth routes are protected except for POST to /auth/login and POST /auth/signup
